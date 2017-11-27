c88378d5226917511d22e756f50c800a760a2234
Dmitry Chaban <dmitrychaban@yahoo.com>
2017-11-27 08:56:21 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/snakepit.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/snakepit.dir/main.cpp.o"
+  "snakepit.pdb"
+  "snakepit"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/snakepit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+using namespace std;
+
+int main() {
+    sf::Window App(sf::VideoMode(800, 600), "myproject");
+
+    while (App.isOpen()) {
+        sf::Event Event;
+        while (App.pollEvent(Event)) {
+            if (Event.type == sf::Event::Closed)
+                App.close();
+        }
+        App.display();
+    }
+    return 0;
+}
